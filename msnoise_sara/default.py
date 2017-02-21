bf1358f02e27a67aa67d8a6ed252cd2be783913a
ThomasLecocq <thomas.lecocq@seismology.be>
2017-02-21 10:14:37 +0100

    at least a realistic default lowpass value for 20Hz sps

--- msnoise_sara/default.py
+++ msnoise_sara/default.py
@@ -5,7 +5,7 @@ default['analysis_duration'] = ["Duration of the Analysis (total in seconds : 36
 default['preprocess_sampling_rate'] = ["Sampling Rate for the SARA PreProcessing [20.0]",'20.0']
 default['resampling_method'] = ["Resampling method [Resample]/Decimate",'Resample']
 default['decimation_factor'] = ["If Resampling mether=Decimate, decimation factor [5]",'5']
-default['preprocess_lowpass'] = ["Preprocessing Low-pass value in Hz [15.0]",'15.0']
+default['preprocess_lowpass'] = ["Preprocessing Low-pass value in Hz [9.0]",'9.0']
 default['preprocess_highpass'] = ["Preprocessing High-pass value in Hz [5.0]",'5.0']
 default['preprocess_taper_length'] = ["Length of the taper is seconds (5.0)]",'5.0']
 
